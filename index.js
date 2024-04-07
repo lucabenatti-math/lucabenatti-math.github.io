8cc95595cf281ae7d7d444cfcbd158a5ed964d7b
lucabenatti-math <149170533+lucabenatti-math@users.noreply.github.com>
2024-04-07 23:54:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,166 @@
+const menuButton = document.querySelector(".news-button");
+/*==== BioNews swiper parallax ====*/
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".menu a");
+
+
+const papersButton = document.querySelector(".papers-button");
+
+document.addEventListener("touchstart", ()=>{
+    navLinks.forEach(link =>{
+        link.classList.remove("non-touch");
+    });
+}, false);
+
+document.addEventListener("mouseover", ()=>{
+    navLinks.forEach(link =>{
+        link.classList.add("non-touch");
+    });
+}, false);
+window.addEventListener("scroll", function() {
+    const parallaxBg = document.querySelector(".parallax-bg");
+    let scrollPosition = window.pageYOffset;
+    parallaxBg.style.top= -20 - scrollPosition * .5 + "px";
+    const header =document.querySelector("header");
+    this.scrollY >=10 ? header.classList.add("scrolled") : header.classList.remove("scrolled");
+    
+  
+    sections.forEach(section => {
+        let top = this.scrollY;
+        let offset = section.offsetTop -150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+        if(top>=offset && top<offset+height){
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                if(id === "Papers")
+                    id="Research";
+                if(id!=="About" && id!=="Home")
+                    document.querySelector(".menu a[href*="+id+"]").classList.add("active");
+                if(id !== "Papers" && id !== "Research"){
+                    papersButton.style.transform = "scale(1)";
+                    document.querySelector(".papers-list").style.height=0;
+                }
+                if(id!=="Home"){
+                    backToBio();
+                }
+            });
+        }
+    });
+});
+
+
+
+papersButton.addEventListener("click", function () {
+    const papersList = document.querySelector(".papers-list");
+    if(papersList.clientHeight){
+        papersList.style.height=0;
+        this.style.transform ="scale(1)";
+    } else {
+        const papersWrapper = document.getElementById("wrapperPapers");
+        this.style.transform ="scale(-1)";
+        papersList.style.height = "calc("+papersWrapper.clientHeight+"px + 1rem)";
+    }
+});
+
+
+function formatDate(inputDate) {
+    // Create a Date object from the input date string
+    const date = new Date(inputDate);
+
+    // Define months array
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    // Extract day, month, and year from the Date object
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+
+    // Format the month using the months array
+    const month = months[monthIndex];
+
+    // Construct the formatted date string
+    const formattedDate = day + " " + month + " " + year;
+
+    // Return the formatted date string
+    return formattedDate;
+}
+
+const bioSwiper = new Swiper(".bio-news", {
+    speed: 600,
+    mousewheel: false,
+    initialSlide:0,
+    direction:"horizontal",
+    effect: "creative",
+    allowTouchMove:false,
+    grab:false,
+    resistance:true,
+    nested: true,
+    resistanceRatio:0,
+    ///slideToClickedSlide:true,
+    creativeEffect: {
+        next: {
+            translate: ["-4rem",0,1],
+        },
+        prev: {
+            translate: [0, 0, -50],
+        },
+    },
+
+});
+
+var theNews = document.querySelectorAll(".news-animation")
+
+theNews.forEach((myNews,index) => {
+    var delay = index * .5;
+    myNews.style.setProperty("--after-animation-delay", delay+"s");
+});
+
+// DISABLE MOUSEWHEEL MOVEMENT WHEN ON SCROLLED ON NEWS
+//var news = document.querySelector(".news");
+//news.addEventListener("mouseenter", () => {
+//    homeSwiper.mousewheel.disable();
+//});
+//
+//news.addEventListener("mouseleave", () => {
+//    homeSwiper.mousewheel.enable();
+//});
+var newsSwiper = new Swiper(".newslist", {
+    speed: 600,
+    initialSlide:0,
+    direction:"vertical",
+    freeMode:true,
+    spaceBetween: "-1px",
+    slidesPerView: "auto",
+    allowTouchMove:true,
+    grab:false,
+    mousewheel: {
+        invert: false,
+        forceToAxis: true,      // Enable forceToAxis for smoother scrolling
+        releaseOnEdges: true,   // Allow releasing on edges for smoother bounce effect
+        sensitivity:.1,
+    },
+    resistance:true,
+    resistanceRatio:0,
+    scrollbar: {
+        el: ".news-scrollbar",
+        hide:true,
+        draggable:true,
+    },
+});
+
+
+function openMenu() {
+    menuButton.classList.toggle("cross");
+    bioSwiper.slideTo(!bioSwiper.realIndex);
+};
+
+
+function backToBio(){
+    bioSwiper.slideTo(0);
+    menuButton.classList.remove("cross");
+}
+
+
+
---
+++ templates.js
@@ -0,0 +1,130 @@
+const researchTemplate = (id,title,description,photo,link) => `
+    <div class="research-line">
+        <div class="research-image">
+            <img src="${photo}">
+        </div>
+        <div class="research-cover">
+            <div style="display: flex;flex-direction: column;gap:.4rem; margin-bottom: 1rem;">
+                <h2>${title}</h2>
+                <p>${description}</p>
+            </div>
+            <svg width="7rem" height="2.5rem">
+                <a href="${link}">
+                    <rect class="rounded-button color" mask="url(#${id}seemore)"  vector-effect="non-scaling-stroke"></rect>
+                    <mask id="${id}seemore">
+                        <rect class="rounded-button"  vector-effect="non-scaling-stroke"></rect>
+                        <text class="button-text" dx="50%" dy="50%">
+                            <tspan>See more</tspan>
+                        </text>
+                    </mask>
+                </a>
+            </svg>
+        </div>
+    </div>
+`;
+
+const paperTemplate = (id,title,coauthors,journal,year,url) => {
+    let coauthorsTemplate =``;
+    if(coauthors!==null){
+        coauthorsTemplate =`with ${coauthors}, `;
+    }
+    return `<li style="font-size:1rem; color:var(--text-light-second);">
+        <a href="${url}" style="color:var(--text-color);text-decoration: underline;font-size:1.1rem;">${title}</a><br/>
+        ${coauthorsTemplate}<em>${journal}</em> (${year}).
+    </li>`
+};
+
+
+const talkTemplate = (id,title,location,date,event,photo,link_slides,link_video) => {
+    let talk_button_slides = ``;
+    let talk_button_video = ``;
+    if(link_slides !== null){
+        talk_button_slides = `<svg class="talk-button" width="5.5rem" height="2.5rem">
+                    <a href="${link_slides}">
+                        <rect class="rounded-button color" mask="url(#${id}talkmask)"  vector-effect="non-scaling-stroke"></rect>
+                        <mask id="${id}talkmask">
+                            <rect class="rounded-button"  vector-effect="non-scaling-stroke"></rect>
+                            <text class="button-text" dx="50%" dy="50%">
+                                <tspan>slides</tspan>
+                            </text>
+                        </mask>
+                    </a>
+                </svg>`;
+    }
+    if(link_video !== null){
+        talk_button_video = `<svg class="talk-button" width="5.5rem" height="2.5rem">
+                    <a href="${link_video}">
+                        <rect class="rounded-button color" mask="url(#${id}talkmask-video)"  vector-effect="non-scaling-stroke"></rect>
+                        <mask id="${id}talkmask-video">
+                            <rect class="rounded-button"  vector-effect="non-scaling-stroke"></rect>
+                            <text class="button-text" dx="50%" dy="50%">
+                                <tspan>video</tspan>
+                            </text>
+                        </mask>
+                    </a>
+                </svg>`;
+    }
+    return `<div class="swiper-slide">
+        <div class="overlayed-color"></div>   
+        <div class="talk-wrapper">          
+            <div class="talks-image" >
+                <img src="${photo}"></img>
+            </div>
+            <div class="talks-details">
+                <center><p style="color:var(--text-light);">${location}</p></center>
+                <center><p><b>${date}</b></p></center>
+                <center><h4 style="color:var(--main-color); margin:.7rem 0rem;">${title}</h4></center>
+                <center><p style="color:var(--text-light);"><em>${event}</em></p></center>
+            </div>
+            <div class="talks-buttons">
+                ${talk_button_slides}
+                ${talk_button_video}
+            </div>
+        </div>
+    </div>`;
+};
+
+
+const eventTemplate = (id,title,subtitle,date,content,photo,button_text,button_link) => `
+    <div class="swiper-slide">
+        <div class="organization-content">
+            <h4><b>${date}</b></h4>
+            <h2 style="color:var(--main-color);"> ${title}</h2>
+            <h4 style="color:var(--text-light); font-weight: 300;margin-bottom:.2rem;"><em>${subtitle}</em></h4>
+            <ul>
+                ${content}
+            </ul>
+            <svg class="organization-button" width="14rem" height="2.5rem">
+                <a href="${button_link}">
+                    <rect class="rounded-button color" mask="url(#${id}eventmask)"  vector-effect="non-scaling-stroke"></rect>
+                    <mask id="${id}eventmask">
+                        <rect class="rounded-button"  vector-effect="non-scaling-stroke"></rect>
+                        <text class="button-text" dx="50%" dy="50%">
+                            <tspan>${button_text}</tspan>
+                        </text>
+                    </mask>
+                </a>
+            </svg>
+        </div>
+        <div class="organization-img">
+            <img src="${photo}">
+        </div>
+    </div>
+`;
+
+const yearTemplate = (year) => `
+    <div class="swiper-slide"> 
+        <div class="year">
+            <h3>${year}</h3>
+        </div>
+    </div>
+`;
+
+
+const teachingTemplate = (activities) => `
+    <div class="swiper-slide"> 
+        ${activities}
+    </div>
+`;
+
+
