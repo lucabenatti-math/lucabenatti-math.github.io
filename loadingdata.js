0724e129b64a6723ac94c28dead268d373e25a99
lucabenatti-math <149170533+lucabenatti-math@users.noreply.github.com>
2024-05-27 10:15:11 +0200

    Update loadingdata.js

--- loadingdata.js
+++ loadingdata.js
@@ -110,6 +110,8 @@ async function loadData() {
                     } else if(progress >.8) {
                         autoplayProgress.style.opacity=(1 - progress)*5;
                     }
+                    progressLine.x1.baseVal.value = 3+40*progress;
+                    progressLine.x2.baseVal.value = 83-40*progress;
 
                 }  
             }
